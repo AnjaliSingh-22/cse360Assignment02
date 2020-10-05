68bf90ff1c1de07a28defa1f02a436e5c68540fe
singh <singh@DESKTOP-6RDMNUH>
2020-10-05 12:15:08 -0700

    AddingMachine check

---
+++ cse360Assignment/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,36 @@
+package cse360assignment02;
+
+public class AddingMachine 
+{
+	private int total;
+	  
+	public AddingMachine()
+	{
+	    total = 0;  // not needed - included for clarity
+	}
+	  
+	public int getTotal() 
+	{
+	    return 0;
+	}
+	  
+	public void add(int value) //method used to add
+	{
+	
+	}
+
+	public void subtract(int value)  //method used to subtract 
+	{
+		
+	}
+ 
+	 public String toString()  // this method returns the string representation
+	 {
+	    return "";
+	 }
+
+	  public void clear() 
+	  {
+		  
+	  }
+}
